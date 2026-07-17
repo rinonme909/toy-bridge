519114c5476bffbcf4bcca8b7c2121c60dd80089
rinonme909 <rinonme909@gmail.com>
2026-07-18 05:08:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 let pendingCmd = null;
 
 app.get('/toy-cmd', (req, res) => {
